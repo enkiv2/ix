957e7fd28ca965323ca297b8fa3e73c0ad7db088
John Ohno <john.ohno@gmail.com>
2011-05-10 14:04:16 -0400

    Getting IDT working part 3: All of the assembler files have been consolidated into start.asm as a way of avoiding some of the silly linking errors. This file will be cleaned up.

--- src/start.asm
+++ src/start.asm
@@ -1,3 +1,98 @@
+;
+; entry.asm
+;
+;	(c) 2005 by Alexander Panek & Igor Galic
+;
+
+; Multiboot header
+MULTIBOOT_PAGE_ALIGN   equ 1<<0
+MULTIBOOT_MEMORY_INFO  equ 1<<1
+
+MULTIBOOT_HEADER_MAGIC equ 0x1BADB002
+MULTIBOOT_HEADER_FLAGS equ MULTIBOOT_PAGE_ALIGN | MULTIBOOT_MEMORY_INFO
+CHECKSUM               equ -(MULTIBOOT_HEADER_MAGIC + MULTIBOOT_HEADER_FLAGS)
+
+align 4                         ;
+
+MultibootHeader:                
+	dd MULTIBOOT_HEADER_MAGIC   ;
+	dd MULTIBOOT_HEADER_FLAGS   ;
+	dd CHECKSUM                 ;
+
+; globals
+global entry                    ;
+global gdt_pointer              ;
+global gdt
+; externs
+extern kmain                    ;
+
+stacksize equ 0x4000            ;
+
+entry:
+    cli                         ; first - to be sure - disable interrupts
+    
+    mov esp, stack + stacksize  ; set up stack pointer
+	
+    push eax					; magic value
+	push ebx					; address of the multiboot information structure
+    
+    call flushGDT               ; ready?
+                                ; set...
+    call kmain					; go!
+
+    hlt                         ; just in case someone
+    for: jmp for                ; is doing something dirty!
+
+section .bss                    ;
+align 32                        ;
+
+stack:
+	resb stacksize              ; reserve stack area
+
+section .data                   ;
+gdt:
+    dd 0x00000000               ; null descriptor
+    dd 0x00000000               ; - 
+    
+    dd 0x0000FFFF               ; kernel code descriptor
+    dd 0x00CF9A00               ; -
+    
+    dd 0x0000FFFF               ; kernel data descriptor
+    dd 0x00CF9200               ; -
+    
+    ; TODO:
+    ;.code_desc_modules:
+    ;.data_desc_modules:
+    ;.code_desc_user:
+    ;.data_desc_user:
+    
+gdt_pointer:
+    dw 0x17                     ; gdt limit [(3 * sizeof(GDTDescriptor)) - 1]
+    dd gdt                      ; gdt base address
+
+; vim: set ts=4 sw=4 et:
+;
+;	gdt.asm
+;
+;		(c) 2005 by Alexander Panek & Igor Galic
+;
+
+; globals
+global flushGDT                 ;
+
+flushGDT:
+    lgdt [gdt_pointer]          ; store the gdt_pointer in GDTR
+    jmp 0x08:flush              ; jump to the new code segment (0x08 = GDT offset)
+flush:
+    mov ax, 0x10                ; re-set the segment registers
+    mov ds, ax                  ;
+    mov es, ax                  ;
+    mov fs, ax                  ;
+    mov gs, ax                  ;
+    mov ss, ax                  ;
+    ret                         ; go back..obviously.
+ 
+; vim: set ts=4 sw=4 et:
 ; bkerndev - Bran's Kernel Development Tutorial
 ; By:   Brandon F. (friesenb@gmail.com)
 ; Desc: Kernel entry point, stack, and Interrupt Service Routines.
@@ -10,317 +105,269 @@
 ; are disabled at this point: More on interrupts later!
 [BITS 32]
 global start
-start:
-    mov esp, _sys_stack     ; This points the stack to our new stack area
-    jmp stublet
 
 ; This part MUST be 4byte aligned, so we solve that issue using 'ALIGN 4'
 ALIGN 4
 mboot:
-    ; Multiboot macros to make a few lines later more readable
-    MULTIBOOT_PAGE_ALIGN	equ 1<<0
-    MULTIBOOT_MEMORY_INFO	equ 1<<1
-    MULTIBOOT_AOUT_KLUDGE	equ 1<<16
-    MULTIBOOT_HEADER_MAGIC	equ 0x1BADB002
-    MULTIBOOT_HEADER_FLAGS	equ MULTIBOOT_PAGE_ALIGN | MULTIBOOT_MEMORY_INFO | MULTIBOOT_AOUT_KLUDGE
-    MULTIBOOT_CHECKSUM	equ -(MULTIBOOT_HEADER_MAGIC + MULTIBOOT_HEADER_FLAGS)
-    EXTERN code, bss, end
 
-    ; This is the GRUB Multiboot header. A boot signature
-    dd MULTIBOOT_HEADER_MAGIC
-    dd MULTIBOOT_HEADER_FLAGS
-    dd MULTIBOOT_CHECKSUM
     
-    ; AOUT kludge - must be physical addresses. Make a note of these:
-    ; The linker script fills in the data for these ones!
-    dd mboot
-    dd code
-    dd bss
-    dd end
-    dd start
-
-; This is an endless loop here. Make a note of this: Later on, we
-; will insert an 'extern _main', followed by 'call _main', right
-; before the 'jmp $'.
-stublet:
-    extern _main
-    call _main
-    jmp $
-
-; This will set up our new segment registers. We need to do
-; something special in order to set CS. We do what is called a
-; far jump. A jump that includes a segment as well as an offset.
-; This is declared in C as 'extern void gdt_flush();'
-global _gdt_flush
-extern _gp
-_gdt_flush:
-    lgdt [_gp]
-    mov ax, 0x10
-    mov ds, ax
-    mov es, ax
-    mov fs, ax
-    mov gs, ax
-    mov ss, ax
-    jmp 0x08:flush2
-flush2:
-    ret
 
 ; Loads the IDT defined in '_idtp' into the processor.
 ; This is declared in C as 'extern void idt_load();'
-global _idt_load
-extern _idtp
-_idt_load:
-    lidt [_idtp]
+global idt_load
+extern idtp
+idt_load:
+    lidt [idtp]
     ret
 
 ; In just a few pages in this tutorial, we will add our Interrupt
 ; Service Routines (ISRs) right here!
-global _isr0
-global _isr1
-global _isr2
-global _isr3
-global _isr4
-global _isr5
-global _isr6
-global _isr7
-global _isr8
-global _isr9
-global _isr10
-global _isr11
-global _isr12
-global _isr13
-global _isr14
-global _isr15
-global _isr16
-global _isr17
-global _isr18
-global _isr19
-global _isr20
-global _isr21
-global _isr22
-global _isr23
-global _isr24
-global _isr25
-global _isr26
-global _isr27
-global _isr28
-global _isr29
-global _isr30
-global _isr31
+global isr0
+global isr1
+global isr2
+global isr3
+global isr4
+global isr5
+global isr6
+global isr7
+global isr8
+global isr9
+global isr10
+global isr11
+global isr12
+global isr13
+global isr14
+global isr15
+global isr16
+global isr17
+global isr18
+global isr19
+global isr20
+global isr21
+global isr22
+global isr23
+global isr24
+global isr25
+global isr26
+global isr27
+global isr28
+global isr29
+global isr30
+global isr31
 
 ;  0: Divide By Zero Exception
-_isr0:
+isr0:
     cli
     push byte 0
     push byte 0
     jmp isr_common_stub
 
 ;  1: Debug Exception
-_isr1:
+isr1:
     cli
     push byte 0
     push byte 1
     jmp isr_common_stub
 
 ;  2: Non Maskable Interrupt Exception
-_isr2:
+isr2:
     cli
     push byte 0
     push byte 2
     jmp isr_common_stub
 
 ;  3: Int 3 Exception
-_isr3:
+isr3:
     cli
     push byte 0
     push byte 3
     jmp isr_common_stub
 
 ;  4: INTO Exception
-_isr4:
+isr4:
     cli
     push byte 0
     push byte 4
     jmp isr_common_stub
 
 ;  5: Out of Bounds Exception
-_isr5:
+isr5:
     cli
     push byte 0
     push byte 5
     jmp isr_common_stub
 
 ;  6: Invalid Opcode Exception
-_isr6:
+isr6:
     cli
     push byte 0
     push byte 6
     jmp isr_common_stub
 
 ;  7: Coprocessor Not Available Exception
-_isr7:
+isr7:
     cli
     push byte 0
     push byte 7
     jmp isr_common_stub
 
 ;  8: Double Fault Exception (With Error Code!)
-_isr8:
+isr8:
     cli
     push byte 8
     jmp isr_common_stub
 
 ;  9: Coprocessor Segment Overrun Exception
-_isr9:
+isr9:
     cli
     push byte 0
     push byte 9
     jmp isr_common_stub
 
 ; 10: Bad TSS Exception (With Error Code!)
-_isr10:
+isr10:
     cli
     push byte 10
     jmp isr_common_stub
 
 ; 11: Segment Not Present Exception (With Error Code!)
-_isr11:
+isr11:
     cli
     push byte 11
     jmp isr_common_stub
 
 ; 12: Stack Fault Exception (With Error Code!)
-_isr12:
+isr12:
     cli
     push byte 12
     jmp isr_common_stub
 
 ; 13: General Protection Fault Exception (With Error Code!)
-_isr13:
+isr13:
     cli
     push byte 13
     jmp isr_common_stub
 
 ; 14: Page Fault Exception (With Error Code!)
-_isr14:
+isr14:
     cli
     push byte 14
     jmp isr_common_stub
 
 ; 15: Reserved Exception
-_isr15:
+isr15:
     cli
     push byte 0
     push byte 15
     jmp isr_common_stub
 
 ; 16: Floating Point Exception
-_isr16:
+isr16:
     cli
     push byte 0
     push byte 16
     jmp isr_common_stub
 
 ; 17: Alignment Check Exception
-_isr17:
+isr17:
     cli
     push byte 0
     push byte 17
     jmp isr_common_stub
 
 ; 18: Machine Check Exception
-_isr18:
+isr18:
     cli
     push byte 0
     push byte 18
     jmp isr_common_stub
 
 ; 19: Reserved
-_isr19:
+isr19:
     cli
     push byte 0
     push byte 19
     jmp isr_common_stub
 
 ; 20: Reserved
-_isr20:
+isr20:
     cli
     push byte 0
     push byte 20
     jmp isr_common_stub
 
 ; 21: Reserved
-_isr21:
+isr21:
     cli
     push byte 0
     push byte 21
     jmp isr_common_stub
 
 ; 22: Reserved
-_isr22:
+isr22:
     cli
     push byte 0
     push byte 22
     jmp isr_common_stub
 
 ; 23: Reserved
-_isr23:
+isr23:
     cli
     push byte 0
     push byte 23
     jmp isr_common_stub
 
 ; 24: Reserved
-_isr24:
+isr24:
     cli
     push byte 0
     push byte 24
     jmp isr_common_stub
 
 ; 25: Reserved
-_isr25:
+isr25:
     cli
     push byte 0
     push byte 25
     jmp isr_common_stub
 
 ; 26: Reserved
-_isr26:
+isr26:
     cli
     push byte 0
     push byte 26
     jmp isr_common_stub
 
 ; 27: Reserved
-_isr27:
+isr27:
     cli
     push byte 0
     push byte 27
     jmp isr_common_stub
 
 ; 28: Reserved
-_isr28:
+isr28:
     cli
     push byte 0
     push byte 28
     jmp isr_common_stub
 
 ; 29: Reserved
-_isr29:
+isr29:
     cli
     push byte 0
     push byte 29
     jmp isr_common_stub
 
 ; 30: Reserved
-_isr30:
+isr30:
     cli
     push byte 0
     push byte 30
     jmp isr_common_stub
 
 ; 31: Reserved
-_isr31:
+isr31:
     cli
     push byte 0
     push byte 31
@@ -329,7 +376,7 @@ _isr31:
 
 ; We call a C function in here. We need to let the assembler know
 ; that '_fault_handler' exists in another file
-extern _fault_handler
+extern fault_handler
 
 ; This is our common ISR stub. It saves the processor state, sets
 ; up for kernel mode segments, calls the C-level fault handler,
@@ -347,7 +394,7 @@ isr_common_stub:
     mov gs, ax
     mov eax, esp
     push eax
-    mov eax, _fault_handler
+    mov eax, fault_handler
     call eax
     pop eax
     pop gs
@@ -358,136 +405,136 @@ isr_common_stub:
     add esp, 8
     iret
 
-global _irq0
-global _irq1
-global _irq2
-global _irq3
-global _irq4
-global _irq5
-global _irq6
-global _irq7
-global _irq8
-global _irq9
-global _irq10
-global _irq11
-global _irq12
-global _irq13
-global _irq14
-global _irq15
+global irq0
+global irq1
+global irq2
+global irq3
+global irq4
+global irq5
+global irq6
+global irq7
+global irq8
+global irq9
+global irq10
+global irq11
+global irq12
+global irq13
+global irq14
+global irq15
 
 ; 32: IRQ0
-_irq0:
+irq0:
     cli
     push byte 0
     push byte 32
     jmp irq_common_stub
 
 ; 33: IRQ1
-_irq1:
+irq1:
     cli
     push byte 0
     push byte 33
     jmp irq_common_stub
 
 ; 34: IRQ2
-_irq2:
+irq2:
     cli
     push byte 0
     push byte 34
     jmp irq_common_stub
 
 ; 35: IRQ3
-_irq3:
+irq3:
     cli
     push byte 0
     push byte 35
     jmp irq_common_stub
 
 ; 36: IRQ4
-_irq4:
+irq4:
     cli
     push byte 0
     push byte 36
     jmp irq_common_stub
 
 ; 37: IRQ5
-_irq5:
+irq5:
     cli
     push byte 0
     push byte 37
     jmp irq_common_stub
 
 ; 38: IRQ6
-_irq6:
+irq6:
     cli
     push byte 0
     push byte 38
     jmp irq_common_stub
 
 ; 39: IRQ7
-_irq7:
+irq7:
     cli
     push byte 0
     push byte 39
     jmp irq_common_stub
 
 ; 40: IRQ8
-_irq8:
+irq8:
     cli
     push byte 0
     push byte 40
     jmp irq_common_stub
 
 ; 41: IRQ9
-_irq9:
+irq9:
     cli
     push byte 0
     push byte 41
     jmp irq_common_stub
 
 ; 42: IRQ10
-_irq10:
+irq10:
     cli
     push byte 0
     push byte 42
     jmp irq_common_stub
 
 ; 43: IRQ11
-_irq11:
+irq11:
     cli
     push byte 0
     push byte 43
     jmp irq_common_stub
 
 ; 44: IRQ12
-_irq12:
+irq12:
     cli
     push byte 0
     push byte 44
     jmp irq_common_stub
 
 ; 45: IRQ13
-_irq13:
+irq13:
     cli
     push byte 0
     push byte 45
     jmp irq_common_stub
 
 ; 46: IRQ14
-_irq14:
+irq14:
     cli
     push byte 0
     push byte 46
     jmp irq_common_stub
 
 ; 47: IRQ15
-_irq15:
+irq15:
     cli
     push byte 0
     push byte 47
     jmp irq_common_stub
 
-extern _irq_handler
+extern irq_handler
 
 irq_common_stub:
     pusha
@@ -504,7 +551,7 @@ irq_common_stub:
     mov eax, esp
 
     push eax
-    mov eax, _irq_handler
+    mov eax, irq_handler
     call eax
     pop eax
 
