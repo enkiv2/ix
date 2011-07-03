54345042aa8e0808495916b16f0b253b243c4054
John Ohno <john.ohno@gmail.com>
2011-07-03 17:22:50 -0400

    A little housekeeping

--- src/doc.sh
+++ src/doc.sh
@@ -2,7 +2,7 @@
 for i in */*.c */*.asm */*.h
 do
 	echo "$i:"
-	cat $i | grep '//@'  | sed -e 's/\/\/\@/\n\t/g' | sed -e 's/\:/\t/g'
+	cat $i | grep '//@'  | sed -e 's/\/\/\@/\n\t/g;s/\:/\t/g;s/[\;\{][\t\ ]*\t/\t/g'
 	echo
 
 done
