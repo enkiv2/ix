b14706c3927da26f57604f48191b57fc4a0b6c7f
John Ohno <john.ohno@gmail.com>
2011-07-03 20:48:31 -0400

    Removed intrusive shadowbox on startup

--- src/ix_main.c
+++ src/ix_main.c
@@ -96,8 +96,6 @@ void kmain(unsigned long magic, unsigned long addr) {
 	init_cells();	
 
 	kb_buf=0;	
-	shadowbox(5, 5, 20, 10, "Welcome to ix. Use\n   w\na      d\n   s    to navigate,\ni to edit,\nand h for help", 0x20, 0x02);
-	while (!kb_buf);
 
 	cls();
 	renice(spawn(nav_cells), 6);
